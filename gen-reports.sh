602e0d3f7e27b5b7f125fe2bb9523cd010eace39
ian mulvany <ian@mulvany.net>
2013-03-14 18:18:19 +0000

    full path to python script for Lingon

--- gen-reports.sh
+++ gen-reports.sh
@@ -1,5 +1,5 @@
 # generate reports 
-python todo-generator.py -t t > ~/Dropbox/notes/nv/current-todos.txt 
-python todo-generator.py -t d > ~/Dropbox/notes/nv/current-dones.txt 
-python todo-generator.py -t l > ~/Dropbox/notes/nv/current-later.txt 
-python todo-generator.py -t r > ~/Dropbox/notes/nv/current-reminders.txt 
+python /Users/ian/code/public-code/todo-generator/todo-generator.py -t t > ~/Dropbox/notes/nv/current-todos.txt 
+python /Users/ian/code/public-code/todo-generator/todo-generator.py -t d > ~/Dropbox/notes/nv/current-dones.txt 
+python /Users/ian/code/public-code/todo-generator/todo-generator.py -t l > ~/Dropbox/notes/nv/current-later.txt 
+python /Users/ian/code/public-code/todo-generator/todo-generator.py -t r > ~/Dropbox/notes/nv/current-reminders.txt 
