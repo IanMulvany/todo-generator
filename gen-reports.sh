7a8f7c7e1d212eefa3818aae374f083c13d44c8e
ian mulvany <ian@mulvany.net>
2013-03-14 13:56:10 +0000

    created script for generating action reports

---
+++ gen-reports.sh
@@ -0,0 +1,4 @@
+# generate reports 
+python todo-generator.py -t t > ~/Dropbox/notes/nv/current-todos.txt 
+python todo-generator.py -t d > ~/Dropbox/notes/nv/current-dones.txt 
+python todo-generator.py -t l > ~/Dropbox/notes/nv/current-later.txt 
